13ede7a553db2273b363e2a5ab259e017358abb0
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-14 15:37:55 -0700

    Timestamp

--- Lab7.cpp
+++ Lab7.cpp
@@ -2,7 +2,8 @@
 #include <iomanip>
 using namespace std;
 
-void Reverse(string *&, int); // Reverse strings in array
+void Reverse(string *&, int); // Reverse order of strings in array
+string OutputArray(string *&arrPtr, int arrSize); // Output array on 1 line
 
 int main()
 {
@@ -10,19 +11,29 @@ int main()
     int ARRAY_SIZE = 5;
     string *sArrayPtr = new string[ARRAY_SIZE]; // Allocate array of 5 ints
 
-    Reverse(sArrayPtr, ARRAY_SIZE);                                                    // Call function to populate array
+    *(sArrayPtr + 0) = "John";
+    *(sArrayPtr + 1) = "Jacob";
+    *(sArrayPtr + 2) = "Jingle";
+    *(sArrayPtr + 3) = "Heimer";
+    *(sArrayPtr + 4) = "Schmidt";
+
+    // Output array before reverse
+    cout << "Name array BEFORE reverse: " << OutputArray(sArrayPtr, ARRAY_SIZE) << endl;
     
-    cout << "Sum = " << fixed << setprecision(2) << SumArray(dArrayPtr, ARRAY_SIZE) << endl; // Output sum formatted
+    // Reverse(sArrayPtr, ARRAY_SIZE); // Reverse order of array strings
+
+    // Output array after reverse
+    cout << "Name array AFTER reverse: " << OutputArray(sArrayPtr, ARRAY_SIZE) << endl;
 
     // Deallocate heap elements and nullify pointers
-    delete[] dArrayPtr;
-    dArrayPtr = nullptr;
+    delete[] sArrayPtr;
+    sArrayPtr = nullptr;
 
     return 0;
 }
 
 // Populate array with user input
-void Reverse(double *& arrPtr, int arrSize)
+void Reverse(string *& arrPtr, int arrSize)
 {
     // For array size, loop with cin to get double values
     cout << "Data entry to populate array:" << endl;
@@ -32,4 +43,19 @@ void Reverse(double *& arrPtr, int arrSize)
         cin >> *(arrPtr + i);
     }
     cout << "Data entry complete." << endl;
+}
+
+// Output array on one line
+string OutputArray(string *&arrPtr, int arrSize)
+{
+    // For array size, output each value in some kind of formatted way
+    string output = "{";
+    for (size_t i = 0; i < arrSize; i++)
+    {
+        if (i > 0) output += ", "; // Add ',' and ' ' for each additional value after the first
+        output += *(arrPtr + i);
+    }
+    output += "}";
+
+    return output;
 }
